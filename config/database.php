89ae460f7de8ab4aac9f9a2ef8744099d256ffe7
Jiho Lee <warezio@msn.com>
2018-12-16 23:10:57 -0800

    update env

--- config/database.php
+++ config/database.php
@@ -42,10 +42,10 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'heroku_7f83b2be5a491ea'),
+            'username' => env('DB_USERNAME', 'bb487165db371f'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
